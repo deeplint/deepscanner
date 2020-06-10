6a7d0c550a6bd82744ae738e8de522f5ec57748f
Gavin Yue <yue.yuanyuan@gmail.com>
2020-06-09 19:36:11 -0700

    Add eslint configs

--- test/greeter-spec.ts
+++ test/greeter-spec.ts
@@ -1,11 +1,11 @@
-import { Greeter } from "../src/greeter";
-import * as chai from "chai";
+import { Greeter } from '../src/greeter';
+import * as chai from 'chai';
 
 const expect = chai.expect;
 
-describe("Greeter", () => {
-  it("should greet with message", () => {
-    const greeter = new Greeter("friend");
-    expect(greeter.greet()).to.equal("Bonjour, friend!");
-  });
+describe('Greeter', () => {
+    it('should greet with message', () => {
+        const greeter = new Greeter('friend');
+        expect(greeter.greet()).to.equal('Bonjour, friend!');
+    });
 });
--- test/index-spec.ts
+++ test/index-spec.ts
@@ -1,9 +1,9 @@
-import index = require("../src/index");
-import * as chai from "chai";
+import index = require('../src/index');
+import * as chai from 'chai';
 
 const expect = chai.expect;
-describe("index - Greeter", () => {
-  it("should provide Greeter", () => {
-    expect(index.Greeter).to.not.be.undefined;
-  });
+describe('index - Greeter', () => {
+    it('should provide Greeter', () => {
+        expect(index.Greeter).to.not.be.undefined;
+    })
 });
