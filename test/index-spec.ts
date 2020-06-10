6f3d010e6e3a4596ac1d4bf0535e60e1dee09f44
Gavin Yue <yue.yuanyuan@gmail.com>
2020-06-09 17:56:14 -0700

    First check

---
+++ test/index-spec.ts
@@ -0,0 +1,9 @@
+import index = require("../src/index");
+import * as chai from "chai";
+
+const expect = chai.expect;
+describe("index - Greeter", () => {
+  it("should provide Greeter", () => {
+    expect(index.Greeter).to.not.be.undefined;
+  });
+});
