857e60a52a7db99ffab28577cde7a47474f30ac4
Gavin Yue <yue.yuanyuan@gmail.com>
2020-06-20 00:07:46 -0700

    Update the AWS S3 bucket resource provider

--- aws/src/index.ts
+++ aws/src/index.ts
@@ -1,1 +1,15 @@
-export * from './scanner';
+import _ = require('lodash');
+import { AWSResourceMap } from './aws';
+import { Resource } from '@deeplint/deepscanner-base';
+
+const ResourceMap = _.merge(AWSResourceMap);
+export async function scan(context: { [key: string]: any }): Promise<Resource[]> {
+  let res: Resource[] = [];
+  await Promise.all(
+    Object.keys(ResourceMap).map(async (key) => {
+      const temp = await ResourceMap[key].collect(context);
+      res = res.concat(temp);
+    }),
+  );
+  return res;
+}
