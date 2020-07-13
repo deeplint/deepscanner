1b27efd02501c43af168121cf2da1a6de4d263fb
Gavin Yue <yue.yuanyuan@gmail.com>
2020-07-13 13:05:53 -0700

    Add terraform scanner

---
+++ terraform/test/scanner-spec.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import { scan } from '../src';
+
+describe('scanner', () => {
+  before(function () {
+    // runs once before the first test in this block
+  });
+  after(function () {
+    // runs once after the last test in this block
+  });
+
+  /**
+  it('should return FullRun Instances', async function () {
+    this.timeout(0);
+    const res = await scan({});
+    console.log(JSON.stringify(res, null, '\t'));
+  });
+   **/
+});
