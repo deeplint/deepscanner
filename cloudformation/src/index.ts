abb06c9e73408e01e48cab97ea7dcb7548a607f4
Gavin Yue <yue.yuanyuan@gmail.com>
2020-07-14 17:41:50 -0700

    Add terraform scanner

---
+++ cloudformation/src/index.ts
@@ -0,0 +1,8 @@
+import _ = require('lodash');
+import { Resource } from '@deeplint/deepscanner-base';
+
+
+export async function scan(context: { [key: string]: any }): Promise<Resource[]> {
+  let res: Resource[] = [];
+  return res;
+}
